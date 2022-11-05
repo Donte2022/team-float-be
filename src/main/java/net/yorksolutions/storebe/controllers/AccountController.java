5ca1f72c7c7103ee03c6c97788e6a968159da32c
Michael Hood <michael.brian.hood@gmail.com>
2022-11-05 08:40:10 -0500

    init

---
+++ src/main/java/net/yorksolutions/storebe/controllers/AccountController.java
@@ -0,0 +1,4 @@
+package net.yorksolutions.storebe.controllers;
+
+public class AccountController {
+}
---
+++ src/main/java/net/yorksolutions/storebe/dto/NewAccountRequestDTO.java
@@ -0,0 +1,4 @@
+package net.yorksolutions.storebe.dto;
+
+public class NewAccountRequestDTO {
+}
