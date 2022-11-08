72d37320fc5559d31d1c4a17d983435990c1bc77
joseph <jtownley684@gmail.com>
2022-11-08 16:48:35 -0600

    post Category 2

--- src/main/java/net/yorksolutions/storebe/repositories/ProductRepository.java
+++ src/main/java/net/yorksolutions/storebe/repositories/ProductRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
-    Iterable<Product> deleteAllByCategoriesContains(Long id);
+//    Iterable<Product> deleteAllByCategoriesContains(Long id);
 }
